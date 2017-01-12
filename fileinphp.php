d1f4c88e8ab33619148dfb2083b635b84e27faf2
Awais Zaki <awais.zaki14@gmail.com>
2017-01-12 12:31:54 +0500

    filing

--- fileinphp.php
+++ fileinphp.php
@@ -37,25 +37,57 @@
 
 $newfile =  "text2.txt";
 $newopen = fopen($newfile,"w");
-if($newopen == false){
-    echo "Error in opening file..";
-    exit();
 
-    }
 
-fwrite($newopen, "this is file");
+fwrite($newopen, "this is written by fwrite() function and the size of the file is in Bytes");
+fclose($newopen);
 
-$newread = fopen($newfile,"r");
-if($fileOpen == false){
-    echo "Error in opening file..";
-    exit();
-
-    }
 
 //$newr = fread($newread, filesize($newread));
+
 echo readfile("text2.txt");
+    echo "<h3>Check file already created or not...</h3>";
+    if(file_exists($newfile))
+    {
+        $fielSize = filesize($newfile);
+        $msg = "File Created already!";
+        $msg .= "containing $fielSize bytes";
+        echo "<br/><div style='border:1px solid red;'>".$msg;
+        echo "<br/><b>The file_exists() function check the file is created before or not</b></div>";
+    }
     
-    
+    else{
+        echo "file does not exist";
+    }
+    echo "<h3>check the file name...</h3>";
+         echo "<br/><div style='border:1px solid red;'>";
+        echo "<br/><b>The basename() function returns the name of the file</b></br>";
+        $basename = basename($newfile);
+        echo $basename;
+        echo "</div>";
+
+        $source = "src.txt";
+        $destination = "des.txt";
+
+        $srcOpen = fopen($source,"w");
+        fwrite($srcOpen,"Hellow world I am Source file and now my content will copying in destination file");
+        fclose($srcOpen);
+
+        $desOpen = fopen($destination,"w");
+
+        $copying = copy($source,$destination);
+        echo "<h3>Copy source file to destination file...</h3>";
+        echo "<br/> <br/><div style='border:1px solid red;'>";
+        echo "<br/>Content of soucrce.txt file <br/>".readfile($source); 
+
+         echo "<br/> <br/>";
+        echo "<br/>Content of destination.txt file <br/>".readfile($destination); 
+
+        echo "<br/><b>The above content is copyied through copy() function</b></div>";
+
+
+
+       
     ?> 
 
 </body>
